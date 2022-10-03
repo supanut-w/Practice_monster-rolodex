73c556a1324b36e7dcaf8f4adc6f37a7a88ecf71
supanut <supanut@gmail.com>
2022-10-03 22:55:17 +0700

    refactor: move search-box as compoenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -43,13 +44,12 @@ class App extends Component {
 
     return (
       <div className="App">
-      <input 
-        className='search-box'
-        type='search'
-        placeholder='search monsters'
-        onChange={ onSearchChange }
-      />
-      <CardList monsters={filteredMonsters} />
+        <SearchBox
+          className='search-box'
+          placeholder='search monsters' 
+          onChangeHandler={onSearchChange} 
+        />
+        <CardList monsters={filteredMonsters} />
     </div>
     );
   }
